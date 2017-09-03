af2f45d57c27446199d5c5c36f3c733a81613745
Austyn Trace <atrace@gmail.com>
2017-09-03 01:47:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element){
+function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
 };
 
-function destructivelyAddElementToBeginningOfArray(array, element){
+function addElementToBeginningOfArray(array, element){
   const newArray = [element, ...array];
   return newArray;
 }
